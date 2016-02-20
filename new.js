d90f940abada868d9d28931377cef09cc1a852d1
Tathagat Nawadia <tathagatnawadia1234@yahoo.com>
2016-02-20 19:55:22 +0530

    7:55PM

--- new.js
+++ new.js
@@ -1,71 +1,92 @@
-var express = require("express"); 
-var http = require("http");
-var util = require("util");
-var MongoClient = require("mongodb").MongoClient; 
-var assert = require("assert");
+var express = require("express"); //AIM 1
+var http = require("http"); //AIM 2
+var util = require("util"); //AIM 3
 
+
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// AIM 1 : Prepare a http server with express
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+//Difference between polling and sockets
+var app = express();
+var server = http.createServer(app);
+
+//Static files middleware
+app.use(express.static("./public"));
+
+server.listen(3000);
+*/
+
+//************************************************************************************************************************************
+
+
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// AIM 2 : Install the socket.io npm module and make basic chat app
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+//$npm install socket.io --save
 var app = express();
 var server = http.createServer(app);
 var io = require('socket.io')(server);
 
 app.use(express.static("./public"));
 
-/*
-Running Python Script Tagger from here
-*/
-var PythonShell = require('python-shell');
-
-var options = {
-	args: ['Tathagat is so bored here that he ordered pizza from Dominoes.']
-};
-
-PythonShell.run('taggers/tagger.py', options,function (err, results) {
-	if (err) throw err;
-	// results is an array consisting of messages collected during execution 
-	console.log('results: %j', results);
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('chat message', function(msg){
+		console.log('message: ' + msg);
+		io.emit('chat message', msg);
+	});
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
 });
+server.listen(3000, function(){
+	console.log('listening on *:3000');
+});
+*/
+
+
+//************************************************************************************************************************************
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// AIM 3 : Adding a few things to the app
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io')(server);
+
+app.use(express.static("./public"));
 
 io.on('connection', function(socket){
-	/*
-	Various event listeners for the app
-	*/
-	
-	socket.on('signIn', function (obj) {
-		var email = obj.email;
-		var lat = obj.latitude;
-		var long = obj.longitude;
-		util.log(email + " just connected !!");
+	socket.on('introduce', function (name) {
+		util.log(name + " just connected !!");
 	});
-	
-	socket.on('articleOpen', function(obj){
-		util.log(obj)
+	socket.on('chat message', function(name,msg){
+		util.log('Tracing :: '+ name + " :: " + msg);
+		io.emit('chat message',name,msg);
 	});
-	
-	socket.on('articleClose', function(obj){
-		util.log(obj)
-	});
-	
-	socket.on('headLines', function(obj){
-		util.log(obj);
-	});
-	
-	
-	socket.on('article click off', function (email) {
-		
+	socket.on('creepy', function (name,msg) {
+		util.log('Creeping :: '+ name + " :: " + msg);
 	})
-	
-	socket.on('hover', function (email,article_text) {
-		
-	});
-	
 	socket.on('disconnect', function(){
 		util.log('user disconnected');
 	});
-	
 });
 server.listen(3000, function(){
-	console.log('News Digest Starting on *:3000');
+	console.log('listening on *:3000');
 });
+*/
 
+//************************************************************************************************************************************
 
 
